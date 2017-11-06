a86749cf081cb23da390d4382d30fd88ef34c649
Kevin Smith <kevincaulfield630@gmail.com>
2017-11-06 22:32:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const app = "I don't do much."
 
-function arrays() {
+function kittens() {
   return ["Milo", "Otis", "Garfield"]
 }
 
-function kittens() {
-  return ["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(name) {
+  kittens.shift("Ralph")
+  return kittens
 }
